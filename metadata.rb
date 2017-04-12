108631c1ff87e4b2c1591b767744aef2607e88d9
Richard Shade <rshade@rightscale.com>
2017-04-12 15:58:14 -0500

    updating for newest everything

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Deploys and configures PHP-based applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.2"
+version          "3.0.0"
+issues_url	 "
 
-chef_version '>= 12.0' if respond_to?(:chef_version)
-depends "application", "~> 4.1"
+chef_version '>= 12.7' if respond_to?(:chef_version)
+depends "application"
 depends "apache2"
 depends "php"
